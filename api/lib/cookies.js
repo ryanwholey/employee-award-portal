32aefe8c5d77c3225c506e9e72a37cf9114cee73
Ryan Wholey <rjwholey@gmail.com>
2019-02-02 00:47:17 -0800

    Add auth lib tests (#5)

--- api/lib/cookies.js
+++ api/lib/cookies.js
@@ -1,16 +1,8 @@
 const Cookie = require('universal-cookie')
 
 function parseRequestForCookie(req, cookieName) {
-    let token
-
     const cookies = new Cookie(req.headers.cookie)
-    const cookieToken = cookies.get('eap-token')
-
-    if (cookieToken) {
-        token = cookieToken
-    }
-
-    return token
+    return cookies.get('eap-token')
 }
 
 module.exports = {

c82412a34d34178885b35e31966883ee3c825295
snaardvark <drewsm@oregonstate.edu>
2019-02-11 12:36:41 -0500

    removing knex migration to update isadmin. That update has now been rolled into the db creation script.

--- db/migrations/20190127154402_update-isadmin.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex, Promise) {
-    return knex.raw('ALTER TABLE `users` CHANGE isadmin is_admin BOOLEAN')
-}
-
-exports.down = function(knex, Promise) {
-    return knex.raw('ALTER TABLE `users` CHANGE `is_admin` `isadmin` BINARY')
-}

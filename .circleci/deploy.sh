a511ca65808d66ed5f88d1df39672c00c04496f1
Ryan Wholey <rjwholey@gmail.com>
2019-02-14 21:35:41 -0800

    staing workflow

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,7 +3,9 @@
 set -euxo pipefail
 shopt -s inherit_errexit
 
-pushd employee-award-portal
+cd employee-award-portal
 git pull origin master
+docker-compose -f .circleci/docker-compose.yml build api
 docker-compose -f .circleci/docker-compose.yml down
 docker-compose -f .circleci/docker-compose.yml up --build -d
+

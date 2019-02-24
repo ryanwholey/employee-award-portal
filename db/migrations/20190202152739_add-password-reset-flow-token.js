a10d8fc25c9d1a3e960e758214074df0df2c0ba1
snaardvark <drewsm@oregonstate.edu>
2019-02-24 16:47:41 -0500

    Updated migration scripts to better handle case where target tables already exist. Removed the CS 467 Mid Point Check report from the repo.

--- db/migrations/20190202152739_add-password-reset-flow-token.js
+++ db/migrations/20190202152739_add-password-reset-flow-token.js
@@ -1,14 +1,18 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('password_reset_flow_token', (table) => {
-        table.increments().primary()
-        table.datetime('ctime', 6).defaultTo(knex.fn.now(6))
-        table.datetime('dtime', 6).nullable()
-        table.string('flow_token').notNullable()
-        table.integer('user_id').notNullable()
-        table.foreign('user_id').references('users.id')
-        table.index('user_id')
-    })
+    knex.schema.hasTable('password_reset_flow_token').then(function (exists) {
+        if (!exists) {
+            return knex.schema.createTable('password_reset_flow_token', (table) => {
+                table.increments().primary()
+                table.datetime('ctime', 6).defaultTo(knex.fn.now(6))
+                table.datetime('dtime', 6).nullable()
+                table.string('flow_token').notNullable()
+                table.integer('user_id').notNullable()
+                table.foreign('user_id').references('users.id')
+                table.index('user_id')
+            });
+        }
+    });
 }
 
 exports.down = function(knex, Promise) {
--- db/migrations/20190203181123_add_award_types_table.js
+++ db/migrations/20190203181123_add_award_types_table.js
@@ -1,12 +1,16 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('award_types', (table) => {
-        table.increments().primary()
-        table.datetime('ctime', 6).defaultTo(knex.fn.now(6))
-        table.datetime('mtime', 6).nullable()
-        table.datetime('dtime', 6).nullable()
-        table.string('name').notNullable().unique()
-    })
+    knex.schema.hasTable('award_types').then(function (exists) {
+        if (!exists) {
+            return knex.schema.createTable('award_types', (table) => {
+                table.increments().primary()
+                table.datetime('ctime', 6).defaultTo(knex.fn.now(6))
+                table.datetime('mtime', 6).nullable()
+                table.datetime('dtime', 6).nullable()
+                table.string('name').notNullable().unique()
+            });
+        }
+    });
 }
 
 exports.down = function(knex, Promise) {

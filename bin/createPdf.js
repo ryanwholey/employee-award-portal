fed199d5f97d50e6e13b75cb75f2fae3c4f75576
Ryan Wholey <rjwholey@gmail.com>
2019-03-11 22:54:03 -0700

    Use one pdf award template

--- bin/createPdf.js
+++ bin/createPdf.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 import React from 'react'
 import { render, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import yargs from 'yargs'
+import AwardPdf from '../frontend/components/awardPdf'
 
 const argv = yargs
 .options('filename', {
@@ -39,30 +40,6 @@ const argv = yargs
 })
 .argv
 
-const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: 'white'
-    },
-    container: {
-        backgroundColor: '#E2F9FA',
-        alignItems: 'center',
-        margin: 100,
-        padding: 100,
-        flexGrow: 1,
-        border: 3,
-        opacity: 5
-    },
-    message: {
-        fontFamily: 'Courier-Oblique',
-        paddingBottom: 25
-    },
-    header: {
-        fontFamily: 'Helvetica-Bold',
-        paddingBottom: 15
-    }
-})
-
 const props = _.pick(argv, [
     'awardTypeName',
     'granted',
@@ -71,29 +48,8 @@ const props = _.pick(argv, [
     'signatureUrl',
 ])
 
-const createDoc = (props) => {
-    const {
-        awardTypeName,
-        granted,
-        creatorName,
-        recipientName,
-    } = props
-
-    return (
-        <Document>
-            <Page style={styles.page} orientation='landscape' size='A4'>
-                <View style={styles.container}>
-                    <Text style={styles.header}>Congratulations, {recipientName}!</Text>
-                    <Text style={styles.message}>You've been awarded the {awardTypeName} award by {creatorName}.</Text>
-                    <Text style={styles.date}>{moment(granted).format('lll')}</Text>
-                </View>
-            </Page>
-        </Document>
-    )
-}
-
 const mediaAwardsDir = path.resolve(__dirname, '../media/awards')
 if (!fs.existsSync(mediaAwardsDir)){
     fs.mkdirSync(mediaAwardsDir)
 }
-render(createDoc(props), path.join(mediaAwardsDir, `${argv.filename}.pdf`))
+render(<AwardPdf {...props} />, path.join(mediaAwardsDir, `${argv.filename}.pdf`))
---
+++ frontend/components/AwardPdf.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import moment from 'moment';
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font} from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: 'white'
+  },
+  container: {
+    backgroundColor: '#E2F9FA',
+    alignItems: 'center',
+    margin: 100,
+    padding: 100,
+    flexGrow: 1,
+    border: 3,
+    opacity: 5
+  },
+
+  message: {
+    fontFamily: 'Courier-Oblique',
+    paddingBottom: 25
+  },
+
+  header: {
+    fontFamily: 'Helvetica-Bold',
+    paddingBottom: 15
+  }
+});
+
+const createDoc = (props) => {
+    const {
+        awardTypeName,
+        granted,
+        creatorName,
+        recipientName,
+    } = props
+
+    return (
+      <Document>
+        <Page style={styles.page} orientation='landscape' size='A4'>
+          <View style={styles.container}>
+            <Text style={styles.header}>Congratulations, {recipientName}!</Text>
+            <Text style={styles.message}>You've been awarded the {awardTypeName} award by {creatorName}.</Text>
+            <Text style={styles.date}>{moment(granted).format('lll')}</Text>
+          </View>
+        </Page>
+      </Document>
+    )
+}
+
+export default createDoc

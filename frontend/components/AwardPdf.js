057033ab44812abe4b2ccf4d2771e41dc98a1603
vckelly <vckelly@gmail.com>
2019-03-13 14:48:21 -0600

    CK - Frontend Signature v1

--- frontend/components/AwardPdf.js
+++ frontend/components/AwardPdf.js
@@ -2,6 +2,59 @@ import React from 'react';
 import moment from 'moment';
 import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font} from '@react-pdf/renderer';
 
+// const styles = StyleSheet.create({
+//   page: {
+//     flexDirection: 'row',
+//     backgroundColor: 'white'
+//   },
+//   container: {
+//     backgroundColor: '#E2F9FA',
+//     alignItems: 'center',
+//     margin: 100,
+//     padding: 100,
+//     flexGrow: 1,
+//     border: 3,
+//     opacity: 5
+//   },
+
+//   message: {
+//     fontFamily: 'Courier-Oblique',
+//     paddingBottom: 25
+//   },
+
+//   header: {
+//     fontFamily: 'Helvetica-Bold',
+//     paddingBottom: 15
+//   }
+// });
+
+// const createDoc = (props) => {
+//     const {
+//         awardTypeName,
+//         granted,
+//         creatorName,
+//         recipientName,
+//     } = props
+
+//     return (
+//       <Document>
+//         <Page style={styles.page} orientation='landscape' size='A4'>
+//           <View style={styles.container}>
+//             <Text style={styles.header}>Congratulations, {recipientName}!</Text>
+//             <Text style={styles.message}>You have been awarded the {awardTypeName} award by {creatorName}.</Text>
+//             <Text style={styles.date}>{moment(granted).format('lll')}</Text>
+//           </View>
+//         </Page>
+//       </Document>
+//     )
+// }
+
+//export default createDoc
+
+import React from 'react';
+import moment from 'moment';
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font} from '@react-pdf/renderer';
+
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
@@ -25,6 +78,12 @@ const styles = StyleSheet.create({
   header: {
     fontFamily: 'Helvetica-Bold',
     paddingBottom: 15
+  },
+
+  signature: {
+    maxHeight: 100, 
+    maxWidth: 100
+    //objectPosition: 'right'
   }
 });
 
@@ -43,6 +102,7 @@ const createDoc = (props) => {
             <Text style={styles.header}>Congratulations, {recipientName}!</Text>
             <Text style={styles.message}>You have been awarded the {awardTypeName} award by {creatorName}.</Text>
             <Text style={styles.date}>{moment(granted).format('lll')}</Text>
+            <Image style={styles.signature} src={ url: valid-url, method: 'GET', headers: {}, body: '' }/>
           </View>
         </Page>
       </Document>

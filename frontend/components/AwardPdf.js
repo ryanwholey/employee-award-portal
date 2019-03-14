3dcb973335d1c8c8f4a74c4c393a31747254db56
Ryan Wholey <rjwholey@gmail.com>
2019-03-13 19:32:23 -0700

    Remove duplicate imports

--- frontend/components/AwardPdf.js
+++ frontend/components/AwardPdf.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import moment from 'moment';
-import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font, Image } from '@react-pdf/renderer';
+// import React from 'react';
+// import moment from 'moment';
+// import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font, Image } from '@react-pdf/renderer';
 
 // const styles = StyleSheet.create({
 //   page: {
